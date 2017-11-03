7bedbd97eb5713b99ce992f5417689eef75f9b1a
jack <guangjian.cgj@alibaba-inc.com>
2017-11-03 17:38:29 +0800

    新闻文章抓取

--- src/main/kotlin/com/ak47/cms/cms/task/NewsArticalTask.kt
+++ src/main/kotlin/com/ak47/cms/cms/task/NewsArticalTask.kt
@@ -2,7 +2,6 @@ package com.ak47.cms.cms.task
 
 import com.ak47.cms.cms.api.NewsArticleCrawler
 import com.ak47.cms.cms.dao.NewsArticalRepository
-import com.ak47.cms.cms.entity.NewsArtical
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -20,15 +19,16 @@ class NewsArticalTask {
     fun doSyncNewsArticalata() {
 
         val pageTotal = newsArticleCrawer.pageNoSum
-        for(pageNo in 1..pageTotal) {
+        for (pageNo in 1..pageTotal) {
             println(pageNo)
         }
 
         val articalList = newsArticleCrawer.getPageNewsArticle(1)
         articalList.forEach {
             val url = it.url
+            val count = 0
             val count = newsArticalRepository.countByUrl(url)
-            if(count==0){
+            if (count == 0) {
                 newsArticalRepository.save(it)
             }
         }
