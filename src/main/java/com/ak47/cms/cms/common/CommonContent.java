47153339554d2d87aadc6194be985f7309a397ea
陈明熙 <wb-cmx239369@alibaba-inc.com>
2017-11-04 10:20:25 +0800

    test

--- src/main/java/com/ak47/cms/cms/common/CommonContent.java
+++ src/main/java/com/ak47/cms/cms/common/CommonContent.java
@@ -6,4 +6,6 @@ package com.ak47.cms.cms.common;
 public interface CommonContent {
     static final String PBC_HOST = "http://www.pbc.gov.cn";//央行
     static final String PBC_NEWS = PBC_HOST + "/goutongjiaoliu/113456/113469/11040/index1.html";//央行第一页新闻
+    static final String PBC_DATA_COUNT = PBC_HOST + "/diaochatongjisi/116219/116319/index.html";//央行数据统计
+    static final String PBC_REPORT = PBC_HOST + "/rmyh/106866/index.html";//央行报告下载
 }
