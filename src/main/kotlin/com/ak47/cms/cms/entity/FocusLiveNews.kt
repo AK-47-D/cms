5f049503242c4527c6ef7815e702aa99529385da
jack <guangjian.cgj@alibaba-inc.com>
2017-11-08 18:04:02 +0800

    FocusLiveNews 焦点快讯

---
+++ src/main/kotlin/com/ak47/cms/cms/entity/FocusLiveNews.kt
@@ -0,0 +1,46 @@
+package com.ak47.cms.cms.entity
+
+import java.util.*
+import javax.persistence.*
+
+/**
+ * 实时快讯 */
+
+@Entity
+@Table(name = "focus_live_news", indexes = arrayOf(
+        Index(name = "item_id", unique = true, columnList = "item_id")))
+class FocusLiveNews {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = -1
+
+    @Column(name = "item_id", unique = true)
+    var item_id = ""
+
+    @Column(name = "content")
+    @Lob
+    var content = ""
+
+    @Column(name = "display_time")
+    var display_time = Date()
+
+    @Column(name = "score")
+    var score = -1
+
+
+    /*
+    * a_stock       A 股
+    * commodity     商品
+    * forex         外汇
+    * global        宏观
+    * us_stock      美股
+    * */
+    @Column(name = "type")
+    var type = "a_stock"
+
+
+    var gmtCreated: Date = Date()
+
+    var gmtModified: Date = Date()
+
+}
