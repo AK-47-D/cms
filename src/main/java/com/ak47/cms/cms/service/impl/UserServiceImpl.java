7318f6dd39610d1cd2d9986825361906d296610e
wb-zxx263018 <wb-zxx263018@alibaba-inc.com>
2017-11-09 15:11:29 +0800

    登陆注册

--- src/main/java/com/ak47/cms/cms/service/impl/UserServiceImpl.java
+++ src/main/java/com/ak47/cms/cms/service/impl/UserServiceImpl.java
@@ -1,13 +1,20 @@
 package com.ak47.cms.cms.service.impl;
 
+import com.ak47.cms.cms.common.MD5Util;
 import com.ak47.cms.cms.dao.UserRepository;
 import com.ak47.cms.cms.entity.User;
 import com.ak47.cms.cms.service.UserService;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
 @Service
 public class UserServiceImpl implements UserService {
+
 
     @Autowired
     private UserRepository userRepository;
@@ -15,6 +22,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(User user) {
+        Date now = new Date();
+        user.setGmtCreate(now);
+        user.setGmtModified(now);
+        user.setIsDelete("N");
+        user.setPassword(MD5Util.md5(user.getPassword()));
         userRepository.save(user);
     }
+
+    @Override
+    public boolean doLogin(User user) {
+        if( userRepository.getUserByUserName(MD5Util.md5(user.getUserName())) == null) return false;
+        return true;
+    }
+
+
 }
