7d59baaa554daca2227b0468bb4660b6a5c8e9a0
15868187925 <15868187925@163.com>
2017-11-12 13:16:58 +0800

    order by a.id desc

--- src/main/kotlin/com/ak47/cms/cms/dao/TechArticleRepository.kt
+++ src/main/kotlin/com/ak47/cms/cms/dao/TechArticleRepository.kt
@@ -13,11 +13,11 @@ interface TechArticleRepository : JpaRepository<TechArticle, Long> {
     fun countByUrl(@Param("url") url: String): Int
 
     @Query("select new com.ak47.cms.cms.dto.TechArticleDto( a.id, a.url, a.title, a.simpleContent, a.showContent , a.tagId, b.tagDetail , a.gmtCreate, a.gmtModified ) " +
-            "from TechArticle a, TechArticleTag b where a.tagId = b.tagId")
+            "from TechArticle a, TechArticleTag b where a.tagId = b.tagId order by a.id desc")
     fun listTechArticleDto(page: Pageable): Page<TechArticleDto>
 
     @Query("select new com.ak47.cms.cms.dto.TechArticleDto( a.id, a.url, a.title, a.simpleContent, a.showContent , a.tagId, b.tagDetail , a.gmtCreate, a.gmtModified ) " +
-            "from TechArticle a left join TechArticleTag b on a.tagId = b.tagId where a.title like %:searchText% or a.showContent  like %:searchText%  ")
+            "from TechArticle a left join TechArticleTag b on a.tagId = b.tagId where a.title like %:searchText% or a.showContent  like %:searchText%  order by a.id desc")
     fun searchTechArticleDto(page: Pageable, @Param("searchText") searchText: String): Page<TechArticleDto>
 
 }
