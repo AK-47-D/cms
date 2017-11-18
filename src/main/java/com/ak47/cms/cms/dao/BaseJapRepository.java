2a71ad9ba0ce2548199934448e22323cca12515c
陈明熙 <wb-cmx239369@alibaba-inc.com>
2017-11-18 11:43:45 +0800

    焦点:新增,编辑,列表 新闻添加图片

--- src/main/java/com/ak47/cms/cms/dao/BaseJapRepository.java
+++ src/main/java/com/ak47/cms/cms/dao/BaseJapRepository.java
@@ -2,6 +2,7 @@ package com.ak47.cms.cms.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 /**
  * Created by wb-cmx239369 on 2017/11/6.
  */
+@NoRepositoryBean
 public interface BaseJapRepository<T> extends JpaRepository<T,Long>{
     @Query("select e from #{#entityName} e where e.status = :status and e.isDeleted = 'n'")
     List<T> findCmsPage(@Param("status") Integer status);
--- src/main/java/com/ak47/cms/cms/entity/FocusEvents.java
+++ src/main/java/com/ak47/cms/cms/entity/FocusEvents.java
@@ -1,9 +1,6 @@
 package com.ak47.cms.cms.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
