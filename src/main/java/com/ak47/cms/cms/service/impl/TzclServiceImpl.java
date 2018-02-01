1dce7f75304616a8a5429eaa6f68502cbce62667
陈明熙 <wb-cmx239369@alibaba-inc.com>
2018-02-02 01:05:14 +0800

    CMS

--- src/main/java/com/ak47/cms/cms/service/impl/TzclServiceImpl.java
+++ src/main/java/com/ak47/cms/cms/service/impl/TzclServiceImpl.java
@@ -3,16 +3,13 @@ package com.ak47.cms.cms.service.impl;
 import com.ak47.cms.cms.dao.TzclJpaRepository;
 import com.ak47.cms.cms.dto.TzclDto;
 import com.ak47.cms.cms.entity.Tzcl;
-import com.ak47.cms.cms.enums.ManageCountryEnum;
 import com.ak47.cms.cms.enums.ManageBankEnum;
+import com.ak47.cms.cms.enums.ManageCountryEnum;
 import com.ak47.cms.cms.result.PageResult;
 import com.ak47.cms.cms.result.Result;
 import com.ak47.cms.cms.result.ResultUtils;
 import com.ak47.cms.cms.service.TzclService;
-import org.apache.xpath.operations.NotEquals;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -24,7 +21,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * Created by wb-cmx239369 on 2017/11/6.
@@ -36,7 +32,9 @@ public class TzclServiceImpl implements TzclService{
     @Autowired
     private EntityManager entityManager;
     private final String PRE_TZCL_SQL = "SELECT t.* FROM Tzcl t WHERE t.country = {} AND t.bank = {} and t.{} <> 0 and t.is_deleted = 'n' order by t.open_date desc limit 1,1";
+    private final String TZCL_SQL = "SELECT t.* FROM Tzcl t WHERE t.country = {} AND t.bank = {} and t.{} <> 0 and t.is_deleted = 'n' order by t.open_date desc limit 1";
 
+    private final BigDecimal HUNDRED = new BigDecimal(100);
     @Override
     public Tzcl save(Tzcl tzcl) {
         return tzclJpaRepository.save(tzcl);
@@ -75,8 +73,8 @@ public class TzclServiceImpl implements TzclService{
 //        return ResultUtils.instancePageResult(pageResult.getPageNumber(),pageResult.getPageSize(),tzclJpaRepository.findTableCount(),tzclDtos,"获取成功",true);
     }
 
-    private String getPreTzclSql(String...wheres){
-        StringBuilder sql = new StringBuilder(PRE_TZCL_SQL);
+    private String getPreTzclSql(String sqlStr,String...wheres){
+        StringBuilder sql = new StringBuilder(sqlStr);
         for(String where:wheres){
             sql = sql.replace(sql.indexOf("{"),sql.indexOf("}")+1,where);
         }
@@ -89,49 +87,55 @@ public class TzclServiceImpl implements TzclService{
         List<TzclDto> tzclDtos = new ArrayList<>();
         tzcls.forEach(tt->{
             TzclDto t = new TzclDto(tt);
-            List list = entityManager.createNativeQuery(getPreTzclSql(t.getCountry()+"",t.getBank()+"","r"),Tzcl.class).getResultList();
-            Tzcl preR = list!=null&&list.size()>1?(Tzcl)list.get(1):null;
-            t.setPerR(preR==null?"0":preR.getR());
-            list = entityManager.createNativeQuery(getPreTzclSql(t.getCountry()+"",t.getBank()+"","cpi"),Tzcl.class).getResultList();
-            Tzcl preCpi = list!=null&&list.size()>1?(Tzcl)list.get(1):null;
-            t.setPerCpi(preCpi==null?new BigDecimal(0):preCpi.getCpi());
-            list = entityManager.createNativeQuery(getPreTzclSql(t.getCountry()+"",t.getBank()+"","gdp"),Tzcl.class).getResultList();
-            Tzcl preGdp = list!=null&&list.size()>1?(Tzcl)list.get(1):null;
-            t.setPerGdp(preGdp==null?new BigDecimal(0):preGdp.getGdp());
-            list = entityManager.createNativeQuery(getPreTzclSql(t.getCountry()+"",t.getBank()+"","un"),Tzcl.class).getResultList();
-            Tzcl preUn = list!=null&&list.size()>1?(Tzcl)list.get(1):null;
-            t.setPerUn(preUn==null?new BigDecimal(0):preUn.getUn());
+            List list = entityManager.createNativeQuery(getPreTzclSql(PRE_TZCL_SQL,t.getCountry()+"",t.getBank()+"","r"),Tzcl.class).getResultList();
+            Tzcl preR = list!=null&&list.size()>0?(Tzcl)list.get(0):null;
+            t.setPerR(preR==null?"0":getNum(preR.getR()).toString());
+            list = entityManager.createNativeQuery(getPreTzclSql(PRE_TZCL_SQL,t.getCountry()+"",t.getBank()+"","cpi"),Tzcl.class).getResultList();
+            Tzcl preCpi = list!=null&&list.size()>0?(Tzcl)list.get(0):null;
+            t.setPerCpi(preCpi==null?new BigDecimal(0):preCpi.getCpi().multiply(HUNDRED));
+            list = entityManager.createNativeQuery(getPreTzclSql(PRE_TZCL_SQL,t.getCountry()+"",t.getBank()+"","gdp"),Tzcl.class).getResultList();
+            Tzcl preGdp = list!=null&&list.size()>0?(Tzcl)list.get(0):null;
+            t.setPerGdp(preGdp==null?new BigDecimal(0):preGdp.getGdp().multiply(HUNDRED));
+            list = entityManager.createNativeQuery(getPreTzclSql(PRE_TZCL_SQL,t.getCountry()+"",t.getBank()+"","un"),Tzcl.class).getResultList();
+            Tzcl preUn = list!=null&&list.size()>0?(Tzcl)list.get(0):null;
+            t.setPerUn(preUn==null?new BigDecimal(0):preUn.getUn().multiply(HUNDRED));
             if(t.getCpi() == null){
-                t.setCpi(new BigDecimal(0));
-            }
-            if(t.getPerCpi() == null){
-                t.setPerCpi(new BigDecimal(0));
+                list = entityManager.createNativeQuery(getPreTzclSql(TZCL_SQL,t.getCountry()+"",t.getBank()+"","cpi"),Tzcl.class).getResultList();
+                Tzcl cpi = list!=null&&list.size()>0?(Tzcl)list.get(0):null;
+                t.setCpi(cpi==null?new BigDecimal(0):cpi.getCpi().multiply(HUNDRED));
+            }else{
+                t.setCpi(t.getCpi().multiply(HUNDRED));
             }
             t.setCpiDiff(t.getCpi().subtract(t.getPerCpi()));
             if(t.getGdp() == null){
-                t.setGdp(new BigDecimal(0));
-            }
-            if(t.getPerGdp() == null){
-                t.setPerGdp(new BigDecimal(0));
+                list = entityManager.createNativeQuery(getPreTzclSql(TZCL_SQL,t.getCountry()+"",t.getBank()+"","gdp"),Tzcl.class).getResultList();
+                Tzcl gdp = list!=null&&list.size()>0?(Tzcl)list.get(0):null;
+                t.setGdp(gdp==null?new BigDecimal(0):gdp.getGdp().multiply(HUNDRED));
+            }else{
+                t.setGdp(t.getGdp().multiply(HUNDRED));
             }
             t.setGdpDiff(t.getGdp().subtract(t.getPerGdp()));
             if(t.getUn() == null){
-                t.setUn(new BigDecimal(0));
+                list = entityManager.createNativeQuery(getPreTzclSql(TZCL_SQL,t.getCountry()+"",t.getBank()+"","un"),Tzcl.class).getResultList();
+                Tzcl un = list!=null&&list.size()>0?(Tzcl)list.get(0):null;
+                t.setUn(un==null?new BigDecimal(0):un.getUn().multiply(HUNDRED));
+            }else{
+                t.setUn(t.getUn().multiply(HUNDRED));
             }
-            if(t.getPerUn() == null){
-                t.setPerUn(new BigDecimal(0));
+            if(t.getR() != null){
+                t.setR(getNum(t.getR()).toString());
             }
             t.setUnDiff(t.getUn().subtract(t.getPerUn()));
             String[] rs = t.getR().split(",");
             String[] perRs = t.getPerR().split(",");
-            BigDecimal r = new BigDecimal(getNum(rs[0]));
+            BigDecimal r = getNum(rs[0],false);
             for(String str : rs){
-                BigDecimal r1 = new BigDecimal(getNum(str));
+                BigDecimal r1 = getNum(str,false);
                 r = r.compareTo(r1) >0?r:r1;
             }
-            BigDecimal perR = new BigDecimal(getNum(perRs[0]));
+            BigDecimal perR = getNum(perRs[0],false);
             for(String str : perRs){
-                BigDecimal perR1 = new BigDecimal(getNum(str));
+                BigDecimal perR1 = getNum(str,false);
                 perR = perR.compareTo(perR1) >0?perR:perR1;
             }
             t.setrDiff(r.subtract(perR));
@@ -146,25 +150,35 @@ public class TzclServiceImpl implements TzclService{
         return tzclDtos;
     }
 
-    private String getNum(String str){
-        Pattern p = Pattern.compile("(\\d+\\.\\d+)");
-        Matcher m = p.matcher(str);
-        if (m.find()) {
-            //如果有相匹配的,则判断是否为null操作
-            //group()中的参数：0表示匹配整个正则，1表示匹配第一个括号的正则,2表示匹配第二个正则,在这只有一个括号,即1和0是一样的
-            str = m.group(1) == null ? "" : m.group(1);
-        } else {
-            //如果匹配不到小数，就进行整数匹配
-            p = Pattern.compile("(\\d+)");
-            m = p.matcher(str);
+    private BigDecimal getNum(String str){
+        return getNum(str,true);
+    }
+    private BigDecimal getNum(String str,boolean isMultiply){
+        try {
+            if(isMultiply)
+                return new BigDecimal(str).multiply(HUNDRED);
+            return new BigDecimal(str);
+        } catch (Exception e) {
+            Pattern p = Pattern.compile("(-)*(\\d+\\.\\d+)");
+            Matcher m = p.matcher(str);
             if (m.find()) {
-                //如果有整数相匹配
-                str = m.group(1) == null ? "" : m.group(1);
+                //如果有相匹配的,则判断是否为null操作
+                //group()中的参数：0表示匹配整个正则，1表示匹配第一个括号的正则,2表示匹配第二个正则,在这只有一个括号,即1和0是一样的
+                str = m.group(0) == null ? "0" : m.group(0);
             } else {
-                //如果没有小数和整数相匹配,即字符串中没有整数和小数，就设为空
-                str = "0";
+                //如果匹配不到小数，就进行整数匹配
+                p = Pattern.compile("(-)*(\\d+)");
+                m = p.matcher(str);
+                if (m.find()) {
+                    //如果有整数相匹配
+                    str = m.group(0) == null ? "" : m.group(0);
+                } else {
+                    //如果没有小数和整数相匹配,即字符串中没有整数和小数，就设为空
+                    str = "0";
+                }
             }
         }
-        return str;
+
+        return new BigDecimal(str);
     }
 }
